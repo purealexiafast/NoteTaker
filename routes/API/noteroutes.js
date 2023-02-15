f932b9ea443c6df8c73e240dcfa0137e50990db8
purealexiafast <106492503+purealexiafast@users.noreply.github.com>
2023-02-15 15:33:23 -0800

    coonected notetaker delete

--- routes/API/noteroutes.js
+++ routes/API/noteroutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router()
+
 const fs = require("fs")
 const path = require("path")
+const { v4: uuidv4 } = require('uuid');
 
 router.get("/", (req, res) => {
     const savednotes = JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/db.json"), "utf-8"))
@@ -10,9 +12,16 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
     console.log(req.body)
     const savednotes = JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/db.json"), "utf-8"))
-    savednotes.push(req.body)
+    savednotes.push({ ...req.body, id: uuidv4() })
     fs.writeFileSync(path.join(__dirname, "../../db/db.json"), JSON.stringify(savednotes))
     res.json(savednotes)
+})
+
+router.delete("/:id", (req, res) => {
+    const savednotes = JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/db.json"), "utf-8"))
+    const newnotes = savednotes.filter(note => note.id !== req.params.id)
+    fs.writeFileSync(path.join(__dirname, "../../db/db.json"), JSON.stringify(newnotes))
+    res.json(newnotes)
 })
 
 module.exports = router
